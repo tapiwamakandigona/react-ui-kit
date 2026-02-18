cc63b2afe0d9ac9b94f7bcea88bf6facfe83c3aa
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-02-18 12:24:19 +0200

    feat: src/hooks/usePrevious.ts

---
+++ src/hooks/usePrevious.ts
@@ -0,0 +1,12 @@
+import { useRef, useEffect } from "react";
+
+/**
+ * Track the previous value of a state variable.
+ * @example
+ * const prevCount = usePrevious(count);
+ */
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+  useEffect(() => { ref.current = value; });
+  return ref.current;
+}
